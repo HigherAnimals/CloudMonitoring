1747590225808f6e545adde877166e4edba55dd5
John Davidson <HigherAnimals@gmail.com>
2012-07-11 07:57:43 -0700

    Adds text field getters; ooh, ahh.

--- src/com/rackspace/cloudmonitoring/AuthenticatorActivity.java
+++ src/com/rackspace/cloudmonitoring/AuthenticatorActivity.java
@@ -2,6 +2,7 @@ package com.rackspace.cloudmonitoring;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AuthenticatorActivity extends Activity {
 
@@ -9,5 +10,15 @@ public class AuthenticatorActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authenticator);
+    }
+
+    private String getUsername() {
+        return ((EditText) findViewById(R.id.usernameField)).getText()
+                .toString();
+    }
+
+    public String getPassword() {
+        return ((EditText) findViewById(R.id.passwordField)).getText()
+                .toString();
     }
 }
